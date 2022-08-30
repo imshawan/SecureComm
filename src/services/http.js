4a6aab6911f3f64f7eae84f4d52417a401165cd9
Shawan Mandal <shawan.sm049@gmail.com>
2022-08-30 23:41:32 +0530

    Added HTTP layer

---
+++ src/services/http.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { APP_REMOTE_HOST } from '../common';
+
+const HEADERS = {
+    "Content-Type" : "application/json",
+    "accept": "*/*"
+  }
+
+export const HTTP = {
+    get: async (uri) => {
+        return new Promise((resolve, reject) => {
+            axios.request({
+                url: APP_REMOTE_HOST + uri,
+                method: 'GET',
+                data: {},
+                headers: HEADERS
+            }).then((response) => {
+                resolve(response.data);
+            }).catch(err => {
+                if (err.response) {
+                    reject(err.response.data);
+                } else {
+                    reject(err.message);
+                }
+              });
+        });
+    },
+    put: (uri, payload={}) => {
+        return new Promise((resolve, reject) => {
+            axios.request({
+                url: APP_REMOTE_HOST + uri,
+                method: 'PUT',
+                data: payload,
+                headers: HEADERS
+            }).then((response) => {
+                resolve(response.data);
+            }).catch(err => {
+                if (err.response) {
+                    reject(err.response.data);
+                } else {
+                    reject(err.message);
+                }
+              });
+        });
+    },
+    post: (uri, payload={}) => {
+        return new Promise((resolve, reject) => {
+            axios.request({
+                url: APP_REMOTE_HOST + uri,
+                method: 'POST',
+                data: payload,
+                headers: HEADERS
+            }).then((response) => {
+                resolve(response.data);
+            }).catch(err => {
+                if (err.response) {
+                    reject(err.response.data);
+                } else {
+                    reject(err.message);
+                }
+              });
+        });
+    },
+    del: (uri) => {
+        return new Promise((resolve, reject) => {
+            axios.request({
+                url: APP_REMOTE_HOST + uri,
+                method: 'DELETE',
+                data: {},
+                headers: HEADERS
+            }).then((response) => {
+                resolve(response.data);
+            }).catch(err => {
+                if (err.response) {
+                    reject(err.response.data);
+                } else {
+                    reject(err.message);
+                }
+              });
+        });
+    },
+};
